0725544602c76a6e189619fc99be8b0ac28edf34
doko-desuka <31810999+doko-desuka@users.noreply.github.com>
2020-03-29 07:18:48 -0300

    0.4.5

--- Default.py
+++ Default.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
-from Lib.Plugin import main
+#from Lib.Plugin import main
 
-main() # See bottom of Plugin.py.
+import xbmcaddon, xbmcgui
+
+ADDON_ICON = xbmcaddon.Addon().getAddonInfo('icon')
+xbmcgui.Dialog().ok(
+    "WatchNixtoons2",
+    "The source website has (temporarily?) blocked all watching for free, now only paid accounts work. " \
+    "If this block is ever lifted we'll update the add-on.\nFor now it's not working, sorry."
+)
+
+# Commented for now. No point in hammering the source website with fruitless requests.
+#main() # See bottom of Plugin.py.
